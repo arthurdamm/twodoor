6cc91cabd2a02002425fb00b1b45948fbb4fc24a
scurry222 <scurry222@gmail.com>
2019-10-11 22:50:07 -0700

    basic card layout

--- static/scripts/card.js
+++ static/scripts/card.js
@@ -1,10 +1,16 @@
+$('.success').hide()
 $(function() {
   $('[name=text-answer]').keydown(function (e) {
     console.log("Enter Answer event");
     if (e.which == 13) {
       $('.front').hide();
+      $('.success').show();
       $('.back').css('visibility', 'visible');
       $('.back').css('position', 'relative');
+      $('.btn--next').css('visibility', 'visible');
     }
   });
+});
+$(".flippable").click(function(){
+  $(this).toggleClass("flipme");
 });
