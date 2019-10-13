313b14f43f08d2970d279b981d4caa4c205731ff
Arthur Damm <indigoarthur@gmail.com>
2019-10-12 22:47:50 -0700

    Initial slide animation

--- static/scripts/card.js
+++ static/scripts/card.js
@@ -1,17 +1,27 @@
 $(function() {
+  let currentDoor = $('.door2');
+
   $('.success').hide();
-  $('.door2').hide();
+  // $('.door2').hide();
   $('[name=text-answer]').keydown(function (e) {
     console.log("Enter Answer event");
     if (e.which == 13) {
-      $('.front').hide();
-      $('.success').show();
-      $('.back').css('visibility', 'visible');
-      $('.back').css('position', 'relative');
+      currentDoor.children('.front').hide();
+      currentDoor.children('.success').show();
+      currentDoor.children('.back').css('visibility', 'visible');
+      currentDoor.children('.back').css('position', 'relative');
       $('.btn--next').css('visibility', 'visible');
     }
   });
-});
-$(".flippable").click(function(){
-  $(this).toggleClass("flipme");
+  
+  $(".flippable").click(function() {
+    $(this).toggleClass('flipme');
+    currentDoor = $(this);
+  });
+
+  $('.btn--next').click(function() {
+    console.log("Sliding:", currentDoor[0])
+    currentDoor.addClass('slide');
+    currentDoor.fadeOut(500);
+  });
 });
