25d699ce859aef4d441e76f6a7b645e2ad72b4a8
Arthur Damm <indigoarthur@gmail.com>
2019-10-12 22:24:42 -0700

    Fix divs, add deck class

--- static/scripts/card.js
+++ static/scripts/card.js
@@ -1,5 +1,6 @@
-$('.success').hide()
 $(function() {
+  $('.success').hide();
+  $('.door2').hide();
   $('[name=text-answer]').keydown(function (e) {
     console.log("Enter Answer event");
     if (e.which == 13) {
