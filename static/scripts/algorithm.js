69919a1e3ffb2b1831ca74b2020f1b4f06fa0ba1
Arthur Damm <indigoarthur@gmail.com>
2019-10-14 16:44:04 -0700

    Implement selectNextCard() algorithm, clean up

--- static/scripts/algorithm.js
+++ static/scripts/algorithm.js
@@ -4,3 +4,21 @@ const matchAnswer = function(answer, card) {
   card.performance.push(result);
   return result;
 }
+
+const selectNextCard = function(deck) {
+  const getfailRate = performance => 1 - (performance.reduce((x, a) => x + a, 0) / performance.length);
+  const failRateList = deck.map(card => getfailRate(card.performance) || 1);
+  console.log("failRateList:", failRateList);
+  const sumFails = failRateList.reduce((x, a) => x + a);
+  console.log("sumFails:", sumFails);
+  const randomBar = Math.random() * sumFails;
+  console.log("randomBar:", randomBar);
+  let i = 0;
+  for (let subSumFails = 0; i < deck.length; i++)
+    if ((subSumFails += failRateList[i]) >= randomBar) {
+      console.log("subSumFails:", subSumFails);
+      break;
+    }
+  console.log("i:", i, )
+  return deck[i];
+}
--- static/scripts/card.js
+++ static/scripts/card.js
@@ -1,31 +1,20 @@
-// import * as datastore from "./datastore.js";
-
 $(function() {
   let nextDoor;
   let currentDoor = $('#door2');
   let animating = false;
   const deck = getDeck();
-
   console.log("getDeck():", deck);
-
   $('[name=text-answer]').focus();
   $('[name=text-answer]').keydown(function (e) {
     if (e.which == 13 && !animating) { //enterkey
-      // if (currentDoor.children('.success').values('visibility', 'visible')) {
       if (matchAnswer($('[name=text-answer]').val(), getCard(currentDoor, deck))) {
         let succStyle = currentDoor.children('.success')[0];
-        // console.log("computed style is " + getComputedStyle(succStyle).visibility);
         if(getComputedStyle(succStyle).visibility == 'hidden') {
-          // currentDoor.children('.front').hide();
           currentDoor.children('.success').css('visibility', 'visible');
           $('.success').hide();
           $('.success').fadeIn(500, "swing");          
           $('.success').fadeOut(500, "swing");
-          // $('.front').fadeIn(1000);
-          // $('.back').fadeIn(1000);
           setTimeout(nextDoorEvent, 500);
-          // currentDoor.children('.text-box').css('background', 'green');
-          // win = false;
         } else
           nextDoorEvent();
       currentDoor.children('.back').css('visibility', 'visible');
@@ -36,7 +25,6 @@ $(function() {
     }
   });
 
-  
   $(".flippable").click(function() {
     $(this).toggleClass('flipme');
     $('[name=text-answer]').focus();
@@ -79,6 +67,7 @@ $(function() {
 });
 
 function getNextCard(currentDoor, deck) {
+  return selectNextCard(deck);
   let cardId = (parseInt(currentDoor.attr('card-id')) + 1) % deck.length;
   for (const card of deck)
     if (card.id === cardId)
@@ -89,9 +78,3 @@ function getNextCard(currentDoor, deck) {
 function getCard(currentDoor, deck) {
   return deck[parseInt(currentDoor.attr('card-id'))];
 }
-// var i = 500;
-// setInterval(function () {
-//     $("#stopWatch").html(i);
-//         i--;
-//     }, 1000);
- 
