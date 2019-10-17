e9eb3a2a190df1e5ed875a834fa29c621c49e15f
scurry222 <scurry222@gmail.com>
2019-10-17 09:59:31 -0700

    javascript for animation

---
+++ static/scripts/home.js
@@ -0,0 +1,8 @@
+$(function() {
+  $('.btn.btn--deck1').on('click', function(){
+    $('.btn.btn--deck1').addClass('btn--animated');
+  });
+  $('.btn.btn--deck2').on('click', function(){
+    $('.btn.btn--deck2').addClass('btn--animated');
+  });
+})
