5ca0cf59a5c1f49fcd925f910ab79cc3cc94f429
Arthur Damm <indigoarthur@gmail.com>
2019-12-17 23:14:34 -0800

    Don't show internal custom deck type

--- static/scripts/deck.js
+++ static/scripts/deck.js
@@ -7,7 +7,7 @@ const addDeck = (deck) =>
   $('.deck-container').append(renderDeckSelectorTemplate(deck));
 
 const populateDeckSelectors = () => {
-  for (deck of Object.values(decks)) {
-    addDeck(deck);
-  }
+  for (deck of Object.values(decks))
+    if (deck.name !== decks.CUSTOM.name)
+      addDeck(deck);
 };
