34441eabbbdb126e10c83b15f643c5915ba58d79
scurry222 <scurry222@gmail.com>
2020-05-28 18:00:56 -0700

    styling fixes for desktop

--- static/scripts/settings.js
+++ static/scripts/settings.js
@@ -7,7 +7,6 @@ const settings = () => _settings;
 
 const assignSettings = (settings) => Object.assign(_settings, settings);
 
-
 const putSetting = (key, val) => _settings[key] = val;
 
 const getSetting = (key) => _settings[key];
